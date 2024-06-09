8710945169468dfdd6d0834fc1398ded453a9e46
dularihasinigammampila <dularihasini18@gmail.com>
2024-06-10 01:44:46 +0800

    added files "r", "w" modes

--- prac_03/files.py
+++ prac_03/files.py
@@ -12,7 +12,7 @@ with open("numbers.txt", "r") as file:
     number_one = int(file.readline().strip())
     number_two = int(file.readline().strip())
 result = number_one + number_two
-print(result)
+print(fresult)
 
 total = 0
 with open("numbers.txt", "r") as file:
