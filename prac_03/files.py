1ee5d62fd7962c0abec20e7bd2c6059f2a43a5f7
dularihasinigammampila <dularihasini18@gmail.com>
2024-06-10 01:43:05 +0800

    added password_checker

---
+++ prac_03/files.py
@@ -0,0 +1,21 @@
+name = input("Name: ")
+file = open("name.txt", "w")
+print(name, file=file)
+file.close()
+
+file = open("name.txt", "r")
+name = file.read().strip()
+file.close()
+print(f"Hello {name}")
+
+with open("numbers.txt", "r") as file:
+    number_one = int(file.readline().strip())
+    number_two = int(file.readline().strip())
+result = number_one + number_two
+print(result)
+
+total = 0
+with open("numbers.txt", "r") as file:
+    for line in file:
+        total += int(line.strip())
+print(total)
