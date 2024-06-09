403d8cabea954bf7f417c6d1d654a351cb3fe8ee
dularihasinigammampila <dularihasini18@gmail.com>
2024-06-09 23:21:30 +0800

    importing random

---
+++ prac_03/randoms.py
@@ -0,0 +1,34 @@
+
+# What did you see on line 1?
+# 11
+
+# What was the smallest number you could have seen, what was the largest?
+# smallest - 5
+# largest - 20
+
+# What did you see on line 2?
+# 9
+
+# What was the smallest number you could have seen, what was the largest?
+#smallest - 3
+#largest - 9
+
+# Could line 2 have produced a 4?
+#No
+
+# What did you see on line 3?
+# 3.0191547656605784
+
+# What was the smallest number you could have seen, what was the largest?
+#smallest - 2.639998832716844 (the smallest output possible : 2.5)
+#largest - 5.4372006835780935 (the largest output possible : 5.5)
+
+# Write code, not a comment, to produce a random number between 1 and 100 inclusive
+
+import random
+
+random_number = random.randint(1, 100)
+print(random_number)
+
+
+
