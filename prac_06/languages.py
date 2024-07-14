445db566ef6b7db479d5af61aa05d7e9223e37ef
dularihasinigammampila <dularihasini18@gmail.com>
2024-07-14 17:42:54 +0800

    Language.py added

---
+++ prac_06/languages.py
@@ -0,0 +1,19 @@
+"""
+Start time = 4.30PM
+End time = 5.20PM
+
+"""
+
+from prac_06.programming_language import ProgrammingLanguage
+
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+print(python)
+
+languages = [python, ruby, visual_basic]
+
+print("The dynamically typed languages are:")
+for language in languages:
+    if language.is_dynamic():
+        print(language.name)
