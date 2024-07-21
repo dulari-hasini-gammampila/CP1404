e697b2f0aa9efe187ee9978e370f5e36cc1942b4
dularihasinigammampila <dularihasini18@gmail.com>
2024-07-21 17:07:31 +0800

    programming_language.py updated

--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -7,20 +7,25 @@ Programming Language class with tests.
 class ProgrammingLanguage:
     """Represent information about a programming language."""
 
-    def __init__(self, name, typing, reflection, year):
+    def __init__(self, name, typing, reflection, year, pointer_arithmetic=False):
         """Construct a ProgrammingLanguage from the given values."""
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
+        self.pointer_arithmetic = pointer_arithmetic
 
     def __repr__(self):
         """Return string representation of a ProgrammingLanguage."""
-        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year},Pointer Arithmetic={self.pointer_arithmetic}"
 
     def is_dynamic(self):
         """Determine if language is dynamically typed."""
         return self.typing == "Dynamic"
+
+    def has_pointer_arithmetic(self):
+        """Check if language supports pointer arithmetic."""
+        return self.pointer_arithmetic
 
 
 def run_tests():
@@ -28,9 +33,11 @@ def run_tests():
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    c = ProgrammingLanguage("C++", "Static", False, 1983, True)  # adding a new language
 
-    languages = [ruby, python, visual_basic]
+    languages = [ruby, python, visual_basic, c]
     print(python)
+    print(c) # check the new language work properly
 
     print("The dynamically typed languages are:")
     for language in languages:
